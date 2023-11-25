aa3ea74b935ee0e42701237e4580ebaa2c8684a4
USER <USER@DESKTOP-CHIMO1D>
2023-11-24 23:03:38 -0500

    Carga inicial del proyecto

---
+++ src/main/java/com/mycompany/poo/s2023/pgrup3/POO2S20231PGRUP3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo.s2023.pgrup3;
+
+/**
+ *
+ * @author USER
+ */
+public class POO2S20231PGRUP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
